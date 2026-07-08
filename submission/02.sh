73bed4d495136254298dbd10f9cd5b6e3f61ea1c
Osuji <weezdomosuji@gmail.com>
2026-07-08 05:19:04 +0100

    feat: add PSBT decoding and extraction of txid, output value, and receiver address

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,8 @@
 # Create a native segwit address and get the public key from the address.
+ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+
+# Get the public key from the address
+PUBKEY=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$ADDR" | jq -r '.pubkey')
+
+echo "Address: $ADDR"
+echo "Public Key: $PUBKEY"
