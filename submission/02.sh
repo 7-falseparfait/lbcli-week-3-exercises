ab411f29977416f46ba061e0d2af4cf7d4458b5c
Osuji <weezdomosuji@gmail.com>
2026-07-08 05:26:01 +0100

    feat: create wallet

--- submission/02.sh
+++ submission/02.sh
@@ -1,9 +1,7 @@
 # Create a native segwit address and get the public key from the address.
-bitcoin-cli -regtest createwallet btrustwallet
-ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" bech32)
 
 # Get the public key from the address
-PUBKEY=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$ADDR" | jq -r '.pubkey')
+PUBKEY=$(bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo "$ADDR" | jq -r '.pubkey')
 
-echo "Address: $ADDR"
-echo "Public Key: $PUBKEY"
+echo "$PUBKEY"
